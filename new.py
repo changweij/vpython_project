1535321a18d095e2521565fcdb2217f984d42e01
weichen931118 <149877333+weichen931118@users.noreply.github.com>
2024-06-13 02:25:02 +0800

    Update new.py

--- new.py
+++ new.py
@@ -1,21 +1,26 @@
 from vpython import *
 import numpy as np
 
-# 創建場景
 scene = canvas(width=800, height=600, center=vector(50, -50, 0), background=color.white)
 
-# 定義終點
 x_final = 100
 y_final = -100
 
-# 創建三顆小球
 ball1 = sphere(pos=vector(0, 0, 0), radius=2, color=color.red, make_trail=True)
 ball2 = sphere(pos=vector(0, 0, 0), radius=2, color=color.green, make_trail=True)
 ball3 = sphere(pos=vector(0, 0, 0), radius=2, color=color.blue, make_trail=True)
 
-# 設置時間參數
-t = 0
-dt = 0.01
+ball1.v = vector(0, 0, 0)
+ball2.v = vector(0, 0, 0)
+ball3.v = vector(0, 0, 0)
 
-# 重力加速度
+t = 0
+dt = 0.001
 g = 9.8
+
+while True:
+    rate(1000)
+
+    ball1.v += vector(g/2, -g/2, 0) * dt
+    ball1.pos.x += ball1.v.x * dt
+    ball1.pos.y += ball1.v.y * dt
