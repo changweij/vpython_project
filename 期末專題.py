ed77d53d844ae668a35fff9fcafc09f876cb5654
changweij <75062767+changweij@users.noreply.github.com>
2024-06-12 21:18:10 +0800

    Update 期末專題.py
    Traceback (most recent call last):
      File "/Users/weijchang/Desktop/vpython_pj.py", line 30, in <module>
        ray.pos = ray.pos + ray.v * dt
                            ~~~~~~^~~~
    TypeError: can't multiply sequence by non-int of type 'float'

--- 期末專題.py
+++ 期末專題.py
@@ -5,15 +5,15 @@ scene = canvas(background=vec(0.8, 0.8, 0.8), width=1200, height=300, center=vec
 
 temperature_0 = 40    #攝氏度
 pressure_0 = 1        #標準大氣壓(atm)
-lightspeed = 1
+lightspeed = 1.0        #暫時設為1 (之後調成3e+8)
 theta_0 = 0
 lamda = 4.5e-7
 n_ref = 1 + (0.0000632 + (0.0295 * lamda * lamda / (146 * lamda * lamda - 1)))
 
 
 curve(pos=[vec(-7, 0, 0), vec(13, 0, 0)], color=color.red, radius=0.02)
-ray = sphere(pos = vec(-6, 0, 0), color = color.blue, radius = 0.01, make_trail=True)
-ray.v = (lightspeed * cos(theta_0), lightspeed * sin(theta_0), 0)
+ray = sphere(pos = vec(-6, 1, 0), color = color.blue, radius = 0.05, make_trail=True)
+ray.v = vector(lightspeed * cos(theta_0), lightspeed * sin(theta_0), 0)
 
 
 dt = 0.001
@@ -23,6 +23,7 @@ ray.prev_v = ray.v
 theta_1 = theta_0
 v1 = lightspeed
 v2 = lightspeed
+n_prev = n_air = 1 + (n_ref - 1) * pressure_0 / (1 + (temperature_0 - 15) * (0.0034785))
 while True:
     rate(5000)
     
@@ -32,6 +33,8 @@ while True:
     
     n_air = 1 + (n_ref - 1) * pressure / (1 + (temperature - 15) * (0.0034785))
     
+    print(n_air)
+    
     v2 = lightspeed / n_air
     theta_2 = theta_1 * n_prev / n_air  
     ray.v = (v2 * cos(theta_2), v2 * sin(theta_2), 0)
@@ -39,4 +42,6 @@ while True:
     #update
     n_prev = n_air
     ray.prev_v = ray.v 
-    theta_1 = atan(ray.v.y / ray.v.x)
+    theta_1 = atan(ray.v[1] / ray.v[0]) #v[1]為y座標 v[0]為x座標
+
+    
