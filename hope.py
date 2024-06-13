0e21a352a083605a22a7788696d5552639827673
changweij <75062767+changweij@users.noreply.github.com>
2024-06-13 10:35:21 +0800

    Update hope.py

--- hope.py
+++ hope.py
@@ -24,40 +24,141 @@ ball_2.v = vec(5, 0, 0) # ball initial velocity
 ball_0.a = vec(0, 0, 0) # ball initial acceleration
 ball_1.a = vec(0, 0, 0) # ball initial acceleration
 ball_2.a = vec(0, 0, 0) # ball initial acceleration
-dt = 0.001 # time step
+dt = 0.01 # time step
 
 # original length of the strings : 10m
 
 t = 0
-while True: 
-    
-    rate(1000) # run 1000 times per real second
-    
-    
-    ball_0.a.x = k1 * (ball_1.pos.x - ball_0.pos.x - 10) / m0
-    ball_1.a.x = -k1 * (ball_1.pos.x - ball_0.pos.x - 10) / m1 + k2 * (ball_2.pos.x - ball_1.pos.x - 10) / m1
-    ball_2.a.x = -k2 * (ball_2.pos.x - ball_1.pos.x - 10) / m2
-    
-    ball_0.v.x += ball_0.a.x * dt
-    ball_1.v.x += ball_1.a.x * dt
-    ball_2.v.x += ball_2.a.x * dt
-    
-    ball_0.pos += ball_0.v * dt
-    ball_1.pos += ball_1.v * dt
-    ball_2.pos += ball_2.v * dt
-    center_of_mass.pos.x = (m0 * ball_0.pos.x + m1 * ball_1.pos.x + m2 * ball_2.pos.x) / (m0 + m1 + m2)
-    
-    
-    if(ball_1.pos.x - ball_0.pos.x <= 2 * size):
-        ball_0.v.x = ball_0.v.x * (m0 - m1) / (m0 + m1) + ball_1.v.x * (2 * m1) / (m0 + m1)
-        ball_1.v.x = ball_1.v.x * (m1 - m0) / (m0 + m1) + ball_0.v.x * (2 * m0) / (m0 + m1)
+for i in range (5):
+    if(i == 0):
+        m0 = 10
+        m1 = 10
+        m2 = 10
+        k1 = 1
+        k2 = 1
         
-    if(ball_2.pos.x - ball_1.pos.x <= 2 * size):
-        ball_1.v.x = ball_1.v.x * (m1 - m2) / (m1 + m2) + ball_2.v.x * (2 * m2) / (m1 + m2)
-        ball_2.v.x = ball_2.v.x * (m2 - m1) / (m1 + m2) + ball_1.v.x * (2 * m1) / (m1 + m2)
+        ball_0.pos = vec( -60, height, 0)
+        ball_1.pos = vec( -50, height, 0)
+        ball_2.pos = vec( -40, height, 0)
+        
+        ball_0.v = vec(0, 0, 0) # ball initial velocity
+        ball_1.v = vec(0, 0, 0) # ball initial velocity
+        ball_2.v = vec(5, 0, 0) # ball initial velocity
+        
+        ball_0.a = vec(0, 0, 0) # ball initial acceleration
+        ball_1.a = vec(0, 0, 0) # ball initial acceleration
+        ball_2.a = vec(0, 0, 0) # ball initial acceleration
+        
+    elif(i == 1):
+        m0 = 10
+        m1 = 10
+        m2 = 20
+        k1 = 1
+        k2 = 1
+        
+        ball_0.pos = vec( -60, height, 0)
+        ball_1.pos = vec( -50, height, 0)
+        ball_2.pos = vec( -40, height, 0)
+        
+        ball_0.v = vec(0, 0, 0) # ball initial velocity
+        ball_1.v = vec(0, 0, 0) # ball initial velocity
+        ball_2.v = vec(5, 0, 0) # ball initial velocity
+        
+        ball_0.a = vec(0, 0, 0) # ball initial acceleration
+        ball_1.a = vec(0, 0, 0) # ball initial acceleration
+        ball_2.a = vec(0, 0, 0) # ball initial acceleration
+        
+    elif(i == 2):
+        m0 = 10
+        m1 = 10
+        m2 = 10
+        k1 = 1
+        k2 = 2
+        
+        ball_0.pos = vec( -60, height, 0)
+        ball_1.pos = vec( -50, height, 0)
+        ball_2.pos = vec( -40, height, 0)
+        
+        ball_0.v = vec(0, 0, 0) # ball initial velocity
+        ball_1.v = vec(0, 0, 0) # ball initial velocity
+        ball_2.v = vec(5, 0, 0) # ball initial velocity
+        
+        ball_0.a = vec(0, 0, 0) # ball initial acceleration
+        ball_1.a = vec(0, 0, 0) # ball initial acceleration
+        ball_2.a = vec(0, 0, 0) # ball initial acceleration
     
-    print(ball_1.v.x)
+    elif(i == 3):
+        m0 = 5
+        m1 = 10
+        m2 = 15
+        k1 = 1
+        k2 = 1
+        
+        ball_0.pos = vec( -60, height, 0)
+        ball_1.pos = vec( -50, height, 0)
+        ball_2.pos = vec( -40, height, 0)
+        
+        ball_0.v = vec(0, 0, 0) # ball initial velocity
+        ball_1.v = vec(2.5, 0, 0) # ball initial velocity
+        ball_2.v = vec(5, 0, 0) # ball initial velocity
+        
+        ball_0.a = vec(0, 0, 0) # ball initial acceleration
+        ball_1.a = vec(0, 0, 0) # ball initial acceleration
+        ball_2.a = vec(0, 0, 0) # ball initial acceleration
     
+    elif(i == 4):
+        m0 = 10
+        m1 = 10
+        m2 = 10
+        k1 = 1
+        k2 = 2
+        
+        ball_0.pos = vec( -60, height, 0)
+        ball_1.pos = vec( -50, height, 0)
+        ball_2.pos = vec( -40, height, 0)
+        
+        ball_0.v = vec(0, 0, 0) # ball initial velocity
+        ball_1.v = vec(2.5, 0, 0) # ball initial velocity
+        ball_2.v = vec(5, 0, 0) # ball initial velocity
+        
+        ball_0.a = vec(0, 0, 0) # ball initial acceleration
+        ball_1.a = vec(0, 0, 0) # ball initial acceleration
+        ball_2.a = vec(0, 0, 0) # ball initial acceleration
+        
+    
+    
+    while True: 
+        
+        rate(1000) # run 1000 times per real second
+        
+        
+        ball_0.a.x = k1 * (ball_1.pos.x - ball_0.pos.x - 10) / m0
+        ball_1.a.x = -k1 * (ball_1.pos.x - ball_0.pos.x - 10) / m1 + k2 * (ball_2.pos.x - ball_1.pos.x - 10) / m1
+        ball_2.a.x = -k2 * (ball_2.pos.x - ball_1.pos.x - 10) / m2
+        
+        ball_0.v.x += ball_0.a.x * dt
+        ball_1.v.x += ball_1.a.x * dt
+        ball_2.v.x += ball_2.a.x * dt
+        
+        ball_0.pos += ball_0.v * dt
+        ball_1.pos += ball_1.v * dt
+        ball_2.pos += ball_2.v * dt
+        center_of_mass.pos.x = (m0 * ball_0.pos.x + m1 * ball_1.pos.x + m2 * ball_2.pos.x) / (m0 + m1 + m2)
+        
+        
+        if(ball_1.pos.x - ball_0.pos.x <= 2 * size):
+            ball_0.v.x = ball_0.v.x * (m0 - m1) / (m0 + m1) + ball_1.v.x * (2 * m1) / (m0 + m1)
+            ball_1.v.x = ball_1.v.x * (m1 - m0) / (m0 + m1) + ball_0.v.x * (2 * m0) / (m0 + m1)
+            
+        if(ball_2.pos.x - ball_1.pos.x <= 2 * size):
+            ball_1.v.x = ball_1.v.x * (m1 - m2) / (m1 + m2) + ball_2.v.x * (2 * m2) / (m1 + m2)
+            ball_2.v.x = ball_2.v.x * (m2 - m1) / (m1 + m2) + ball_1.v.x * (2 * m1) / (m1 + m2)
+        
+        print(ball_1.v.x)
+        
+        if(ball_2.pos.x >= 100):
+            break
+
 
 
     
