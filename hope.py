33b78ea292c4dc5895de74a76d5eebdaaa2ef4d2
changweij <75062767+changweij@users.noreply.github.com>
2024-06-13 09:53:38 +0800

    Update hope.py

--- hope.py
+++ hope.py
@@ -9,19 +9,19 @@ k1 = 1
 k2 = 1
 scene = canvas(width = 800, height = 800, center = vec(0,height/2,0), background=vec(0.5,0.5,0)) # open a window
 floor = box(length=30, height=0.01, width=10, color=color.blue) # the floor
-ball_0 = sphere(pos = vec( -10, height, 0), radius = size, color=color.red, make_trail = True, trail_radius = 0.05) # the ball
-ball_1 = sphere(pos = vec( 0, height, 0), radius = size, color=color.green, make_trail = True, trail_radius = 0.05) # the ball
-ball_2 = sphere(pos = vec( 10, height, 0), radius = size, color=color.blue, make_trail = True, trail_radius = 0.05) # the ball
+ball_0 = sphere(pos = vec( -10, height, 0), radius = size, color=color.red) # the ball
+ball_1 = sphere(pos = vec( 0, height, 0), radius = size, color=color.green) # the ball
+ball_2 = sphere(pos = vec( 10, height, 0), radius = size, color=color.blue) # the ball
 #msg = text(text = 'Free Fall', pos = vec(-10, 10, 0))
 
 ball_0.v = vec(0, 0, 0) # ball initial velocity
 ball_1.v = vec(0, 0, 0) # ball initial velocity
-ball_2.v = vec(100, 0, 0) # ball initial velocity
+ball_2.v = vec(5, 0, 0) # ball initial velocity
 
 ball_0.a = vec(0, 0, 0) # ball initial acceleration
 ball_1.a = vec(0, 0, 0) # ball initial acceleration
 ball_2.a = vec(0, 0, 0) # ball initial acceleration
-dt = 0.001 # time step
+dt = 0.0005 # time step
 
 # original length of the strings : 10m
 
@@ -31,17 +31,27 @@ while True:
     rate(1000) # run 1000 times per real second
     
     
-    ball_0.a = k1 * (ball_1.pos.x - ball_0.pos.x - 10) / m0
-    ball_1.a = -k1 * (ball_1.pos.x - ball_0.pos.x - 10) / m1 + k2 * (ball_2.pos.x - ball_1.pos.x - 10) / m1
-    ball_2.a = -k2 * (ball_2.pos.x - ball_1.pos.x - 10) / m2
+    ball_0.a.x = k1 * (ball_1.pos.x - ball_0.pos.x - 10) / m0
+    ball_1.a.x = -k1 * (ball_1.pos.x - ball_0.pos.x - 10) / m1 + k2 * (ball_2.pos.x - ball_1.pos.x - 10) / m1
+    ball_2.a.x = -k2 * (ball_2.pos.x - ball_1.pos.x - 10) / m2
     
-    ball_0.v += ball_0.a * dt
-    ball_1.v += ball_1.a * dt
-    ball_2.v += ball_2.a * dt
+    ball_0.v.x += ball_0.a.x * dt
+    ball_1.v.x += ball_1.a.x * dt
+    ball_2.v.x += ball_2.a.x * dt
     
     ball_0.pos += ball_0.v * dt
     ball_1.pos += ball_1.v * dt
     ball_2.pos += ball_2.v * dt
+    
+    if(ball_1.pos.x - ball_0.pos.x <= 0.5):
+        ball_0.v.x = ball_0.v.x * (m0 - m1) / (m0 + m1) + ball_1.v.x * (2 * m1) / (m0 + m1)
+        ball_1.v.x = ball_1.v.x * (m1 - m0) / (m0 + m1) + ball_0.v.x * (2 * m0) / (m0 + m1)
+        
+    if(ball_2.pos.x - ball_1.pos.x <= 0.5):
+        ball_1.v.x = ball_1.v.x * (m1 - m2) / (m1 + m2) + ball_2.v.x * (2 * m2) / (m1 + m2)
+        ball_2.v.x = ball_2.v.x * (m2 - m1) / (m1 + m2) + ball_1.v.x * (2 * m1) / (m1 + m2)
+    
+    print(ball_1.v.x)
     
 
 
