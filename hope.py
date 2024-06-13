a02404c9aa83255fe58df39449892e55d31f9bfa
changweij <75062767+changweij@users.noreply.github.com>
2024-06-13 10:09:07 +0800

    Update hope.py

--- hope.py
+++ hope.py
@@ -13,6 +13,8 @@ floor = box(length = 200, height = 0.01, width = 10, color = color.black) # the 
 ball_0 = sphere(pos = vec( -60, height, 0), radius = size, color=color.red) # the ball
 ball_1 = sphere(pos = vec( -50, height, 0), radius = size, color=color.green) # the ball
 ball_2 = sphere(pos = vec( -40, height, 0), radius = size, color=color.blue) # the ball
+center_of_mass = arrow(pos=vec(0, height * 2, 0), axis=vec(0, -5, 0), shaftwidth = 2)
+
 #msg = text(text = 'Free Fall', pos = vec(-10, 10, 0))
 
 ball_0.v = vec(0, 0, 0) # ball initial velocity
@@ -43,6 +45,8 @@ while True:
     ball_0.pos += ball_0.v * dt
     ball_1.pos += ball_1.v * dt
     ball_2.pos += ball_2.v * dt
+    center_of_mass.pos.x = (m0 * ball_0.pos.x + m1 * ball_1.pos.x + m2 * ball_2.pos.x) / (m0 + m1 + m2)
+    
     
     if(ball_1.pos.x - ball_0.pos.x <= 2 * size):
         ball_0.v.x = ball_0.v.x * (m0 - m1) / (m0 + m1) + ball_1.v.x * (2 * m1) / (m0 + m1)
@@ -60,4 +64,3 @@ while True:
 #msg.visible = False
 #msg = text(text = str(ball.v.y), pos = vec(-10, 10, 0))
 #print(ball.v.y)
-
